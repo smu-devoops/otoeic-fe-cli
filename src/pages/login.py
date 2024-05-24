6d3f45382f26c13d7db4d1b521093f84823ab3d3
chanmin Lee <dlcksals87@gmail.com>
2024-05-24 17:13:43 +0900

    FEAT : 단어 서비스 구현(랜덤서비스 미구현) + 유저 서비스 구현(리팩토링 필요)

--- src/pages/login.py
+++ src/pages/login.py
@@ -1,13 +1,24 @@
 from services.user import user_service
+from services.word import word_service
 
 from pages.signup import render_signup_page
-
+from services.user import User
+from services.word import Word
 
 def render_login_page():
     # TODO
-    # 만약 사용자가 회원가입을 원한다면...
-    user = user_service.login("이찬민", "이찬민")
-    print(user)
 
-    # render_signup_page()
-    pass
+    # 만약 사용자가 회원가입을 원한다면...
+    # add_word = Word(id=None, english="cat", korean="고양이", type="n", level=1, date_modified=None, date_created=None, user_created=1)
+    # word_service.create(add_word)
+
+    words = word_service.list()
+    # words[-1].english = "upward"
+    # words[-1].korean = "위"
+    # words[-1].type = "n"
+    # words[-1].level = 1
+    # word_service.update(words[-1])
+
+    # word_service.delete(words[-1])
+    # word_service.delete(words[-2])
+    input()
--- src/services/word.py
+++ src/services/word.py
@@ -6,8 +6,8 @@ import random
 import typing
 import requests
 
-from user import User
-from user import user_service
+from services.user import User
+from services.user import user_service
 
 HOST = "http://172.30.1.75:8000"
 
@@ -25,36 +25,53 @@ class Word:
     date_modified: datetime.datetime
     date_created: datetime.datetime
     user_created: User
-
+    
 
 class WordService:
-    def create(self, eng: str, kor: str, type: str, level: str) -> Word:
-        # TODO
-        pass
+
+    def create(self, word:Word) -> Word:
+        add_data = requests.post(HOST + "/word", data={
+            "english": word.english,
+            "korean": word.korean,
+            "type": word.type,
+            "level": word.level,
+            "user_created": word.user_created,
+        })
+        return add_data
 
     def get(self, id: int) -> Word | None:
-        # TODO
-        pass
+        get_words = self.list()
+        return get_words[id]
 
     def list(self) -> typing.List[Word]:
         res = requests.get(HOST + "/word")
         data= res.json()
-        word_list = Word(id=data["id"],english=data["english"],korean=["korean"],type=data["type"],level=data["level"],date_modified=data["date_modified"],date_created=data["date_created"],user_created=data["user_created"])
+        word_list = []
+        for word in data:
+            word_dic=Word(id=word["id"], english=word["english"],korean=["korean"],type=word["type"],level=word["level"],date_modified=word["date_modified"],date_created=word["date_created"],user_created=word["user_created"])
+            word_list.append(word_dic)
 
+        return word_list
 
     def random(self, amount: int, level: str) -> typing.List[Word]:
-        # TODO
         words = self.list()
         random.shuffle(words)
         ...
 
     def update(self, word: Word) -> None:
-        # TODO
-        pass
+        # words = self.list()
+        updata_data = requests.patch(HOST + "/word/" + str(word.id), data={
+            "english": word.english,
+            "korean": word.korean,
+            "type": word.type,
+            "level": word.level,
+        })
 
-    def delete(self, word: Word) -> None:
-        # TODO
-        pass
+        return
+
+    def delete(self, word:Word) -> None:
+        del_data = requests.delete(HOST + "/word/" + str(word.id))
+        return
 
 
 word_service = WordService()
