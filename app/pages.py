542f15772151acdb9b6f1f5183d417e7f125da8f
chanmin Lee <dlcksals87@gmail.com>
2024-05-31 20:34:08 +0900

    feat(pages) : user home page implementation

--- app/pages.py
+++ app/pages.py
@@ -83,5 +83,41 @@ class SignupPage(Page):
 
 
 class UserHomePage(Page):
-    # TODO
+
+    def render(self) -> Page | None:
+        print(
+            "-------------------------사용자 홈 페이지입니다---------------------------------"
+        )
+        menu_num = input(
+            "일반 단어 테스트는 1, 내 수준 점검하기는 2, 단어 관리는 3, 로그아웃을 원하면 4를 입력하세요. "
+        )
+        if menu_num not in [1, 2, 3, 4]:
+            print("메뉴 숫자가 아닙니다.")
+            return UserHomePage()
+        if menu_num == "1":
+            print("일반 단어 테스트 페이지로 이동합니다.")
+            return WordTestPage()
+        if menu_num == "2":
+            print("내 수준 점검하기 페이지로 이동합니다.")
+            return LevelCheckPage()
+        if menu_num == "3":
+            print("단어 관리 페이지로 이동합니다.")
+            return WordManagePage()
+        if menu_num == "4":
+            print("로그인 메뉴 페이지로 이동합니다.")
+            return MenuPage()
+
+
+class WordTestPage(Page):
+    # TODO:
+    pass
+
+
+class LevelCheckPage(Page):
+    # TODO:
+    pass
+
+
+class WordManagePage(Page):
+    # TODO:
     pass
