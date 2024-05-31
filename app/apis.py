93a176649926b9b09007b724fac807a0b722efb0
Hepheir <hepheir@gmail.com>
2024-05-31 18:52:51 +0900

    feat(apis): logout, register 인터페이스 추가

--- app/apis.py
+++ app/apis.py
@@ -5,6 +5,23 @@ from app.models import *
 
 class API:
     def login(self, username: str, password: str) -> Optional[User]:
+        """로그인을 시도한다.
+
+        성공하면 User 객체를 반환한다.
+        실패하면 None을 반환한다.
+        """
+        raise NotImplementedError
+
+    def logout(self) -> None:
+        """로그아웃을 시도한다."""
+        raise NotImplementedError
+
+    def register(self, username: str, password: str) -> Optional[User]:
+        """회원가입을 시도한다.
+
+        성공하면 User 객체를 반환한다.
+        실패하면 None을 반환한다.
+        """
         raise NotImplementedError
 
 
