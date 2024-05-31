c3f005462912e91bab3eeb76012def46484e5602
Hepheir <hepheir@gmail.com>
2024-05-31 19:09:25 +0900

    feat(models): add Word model

--- app/models.py
+++ app/models.py
@@ -13,3 +13,15 @@ class User:
     streak: int = None
     date_created: datetime.datetime = None
     is_staff: int = None
+
+
+@dataclasses.dataclass
+class Word:
+    id: int = None
+    english: str = None
+    korean: str = None
+    type: str = None
+    level: int = None
+    date_modified: datetime.datetime = None
+    date_created: datetime.datetime = None
+    user_created: User = None
