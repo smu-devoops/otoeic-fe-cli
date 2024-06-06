3bfc0700a3a0c976e390338cde12cb128196f07a
Hepheir <hepheir@gmail.com>
2024-06-06 21:14:18 +0900

    refactor(app): Refactor app module structure and update application initialization

--- app/app.py
+++ app/app.py
@@ -1,13 +1,10 @@
 from __future__ import annotations
 
-from . import pages
+from app.pages import *
 
 
 class Application:
-    def __init__(self) -> None:
-        page = pages.MenuPage()
-        self._start(page)
-
-    def _start(self, page: pages.Page):
+    def run(self):
+        page = get_initial_page()
         while page is not None:
             page = page.render()
